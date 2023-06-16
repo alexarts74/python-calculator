a9057e4b2acc3cdf7a0ea4c26122d69bf5ccd363
alexandre artus <artusalexandre@yahoo.fr>
2023-06-16 12:14:54 +0200

    with boucle

--- calculator.py
+++ calculator.py
@@ -1,14 +1,20 @@
-num_1 = input("Veuillez saisir le premier nombre : ")
-num_2 = input ("Veuillez saisir le deuxième nombre : ")
-operator = input("Veuillez choisir un opérateur : ")
+num_1 = num_2 = ""
 
-if operator == "+" and num_1.isdigit and num_2.isdigit:
-    print(f'Le résultat est {int(num_1) + int(num_2)}')
-elif operator == "-" and num_1.isdigit and num_2.isdigit:
-    print(f'Le résultat est {int(num_1) - int(num_2)}')
-elif operator == "*" and num_1.isdigit and num_2.isdigit:
-    print(f'Le résultat est {int(num_1) * int(num_2)}')
-elif operator == "/" and num_1.isdigit and num_2.isdigit:
-    print(f'Le résultat est {int(num_1) / int(num_2)}')
-else:
-    print("Veuillez entrer des valeurs correctes")
+while not (num_1.isdigit() and num_2.isdigit()):
+    num_1 = input("Veuillez saisir le premier nombre : ")
+    num_2 = input ("Veuillez saisir le deuxième nombre : ")
+    operator = input("Veuillez choisir un opérateur : ")
+    if operator == "+" and num_1.isdigit and num_2.isdigit:
+        print(f'Le résultat est {int(num_1) + int(num_2)}')
+        break
+    elif operator == "-" and num_1.isdigit and num_2.isdigit:
+        print(f'Le résultat est {int(num_1) - int(num_2)}')
+        break
+    elif operator == "*" and num_1.isdigit and num_2.isdigit:
+        print(f'Le résultat est {int(num_1) * int(num_2)}')
+        break
+    elif operator == "/" and num_1.isdigit and num_2.isdigit:
+        print(f'Le résultat est {int(num_1) / int(num_2)}')
+        break
+    else:
+        print("Veuillez entrer des valeurs correctes")
